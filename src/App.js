48aa9face1bed12b50e9695a6f12818fc562c768
Julian Gomez <julian.gomez@dat.com>
2021-12-09 20:57:50 -0500

    Navbar con logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Esta es la Barra</h1>
+      <h1>Esta es la Navbar de Bootstrap</h1>
       <NavBar />
     </div>
-    
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import logo from '.././logo.svg';
 
 const NavBar = () => {
 	return (
 		<div>
 			<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
 				<div className="container-fluid">
-					<a className="navbar-brand" href="#">Navbar</a>
+				<img src={logo} className="App-logo" alt="logo" />
+					<a className="navbar-brand" href="#">E-commerce Shop!!</a>
 					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon"></span>
 					</button>
